903375a5f024014a92b05c593b65061e40eb02da
John Park <kyopark2014@gmail.com>
2025-06-27 22:16:48 +0900

    update agent

--- application/utils.py
+++ application/utils.py
@@ -2,6 +2,32 @@ import logging
 import sys
 import json
 import traceback
+import boto3
+import os
+
+from langchain_community.utilities.tavily_search import TavilySearchAPIWrapper
+
+logging.basicConfig(
+    level=logging.INFO,  # Default to INFO level
+    format='%(filename)s:%(lineno)d | %(message)s',
+    handlers=[
+        logging.StreamHandler(sys.stderr)
+    ]
+)
+logger = logging.getLogger("mcp-basic")
+
+def load_config():
+    config = None
+    
+    with open("application/config.json", "r", encoding="utf-8") as f:
+        config = json.load(f)
+    
+    return config
+
+config = load_config()
+
+bedrock_region = config['region']
+projectName = config['projectName']
 
 def get_contents_type(file_name):
     if file_name.lower().endswith((".jpg", ".jpeg")):
@@ -30,20 +56,6 @@ def get_contents_type(file_name):
         content_type = "no info"    
     return content_type
 
-def status(st, str):
-    st.info(str)
-    
-def stcode(st, code):
-    st.code(code)
-
-def load_config():
-    config = None
-    
-    with open("application/config.json", "r", encoding="utf-8") as f:
-        config = json.load(f)
-    
-    return config
-
 def load_mcp_env():
     with open("application/mcp.env", "r", encoding="utf-8") as f:
         mcp_env = json.load(f)
@@ -52,3 +64,99 @@ def load_mcp_env():
 def save_mcp_env(mcp_env):
     with open("application/mcp.env", "w", encoding="utf-8") as f:
         json.dump(mcp_env, f)
+
+# api key to get weather information in agent
+secretsmanager = boto3.client(
+    service_name='secretsmanager',
+    region_name=bedrock_region
+)
+
+# api key for weather
+weather_api_key = ""
+try:
+    get_weather_api_secret = secretsmanager.get_secret_value(
+        SecretId=f"openweathermap-{projectName}"
+    )
+    #print('get_weather_api_secret: ', get_weather_api_secret)
+    secret = json.loads(get_weather_api_secret['SecretString'])
+    #print('secret: ', secret)
+    weather_api_key = secret['weather_api_key']
+
+except Exception as e:
+    raise e
+
+# api key to use Tavily Search
+tavily_key = tavily_api_wrapper = ""
+try:
+    get_tavily_api_secret = secretsmanager.get_secret_value(
+        SecretId=f"tavilyapikey-{projectName}"
+    )
+    #print('get_tavily_api_secret: ', get_tavily_api_secret)
+    secret = json.loads(get_tavily_api_secret['SecretString'])
+    #print('secret: ', secret)
+
+    if "tavily_api_key" in secret:
+        tavily_key = secret['tavily_api_key']
+        #print('tavily_api_key: ', tavily_api_key)
+
+        if tavily_key:
+            tavily_api_wrapper = TavilySearchAPIWrapper(tavily_api_key=tavily_key)
+            #     os.environ["TAVILY_API_KEY"] = tavily_key
+
+        else:
+            logger.info(f"tavily_key is required.")
+except Exception as e: 
+    logger.info(f"Tavily credential is required: {e}")
+    raise e
+
+# api key to use firecrawl Search
+firecrawl_key = ""
+try:
+    get_firecrawl_secret = secretsmanager.get_secret_value(
+        SecretId=f"firecrawlapikey-{projectName}"
+    )
+    secret = json.loads(get_firecrawl_secret['SecretString'])
+
+    if "firecrawl_api_key" in secret:
+        firecrawl_key = secret['firecrawl_api_key']
+        # print('firecrawl_api_key: ', firecrawl_key)
+except Exception as e: 
+    logger.info(f"Firecrawl credential is required: {e}")
+    raise e
+
+# api key to use perplexity Search
+perplexity_key = ""
+try:
+    get_perplexity_api_secret = secretsmanager.get_secret_value(
+        SecretId=f"perplexityapikey-{projectName}"
+    )
+    #print('get_perplexity_api_secret: ', get_perplexity_api_secret)
+    secret = json.loads(get_perplexity_api_secret['SecretString'])
+    #print('secret: ', secret)
+
+    if "perplexity_api_key" in secret:
+        perplexity_key = secret['perplexity_api_key']
+        #print('perplexity_api_key: ', perplexity_api_key)
+
+except Exception as e: 
+    logger.info(f"perplexity credential is required: {e}")
+    raise e
+
+# api key to use LangSmith
+langsmith_api_key = ""
+try:
+    get_langsmith_api_secret = secretsmanager.get_secret_value(
+        SecretId=f"langsmithapikey-{projectName}"
+    )
+    #print('get_langsmith_api_secret: ', get_langsmith_api_secret)
+    secret = json.loads(get_langsmith_api_secret['SecretString'])
+    #print('secret: ', secret)
+    langsmith_api_key = secret['langsmith_api_key']
+    langchain_project = secret['langchain_project']
+except Exception as e:
+    raise e
+
+if langsmith_api_key:
+    os.environ["LANGCHAIN_API_KEY"] = langsmith_api_key
+    os.environ["LANGCHAIN_TRACING_V2"] = "true"
+    os.environ["LANGCHAIN_PROJECT"] = langchain_project
